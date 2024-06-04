ad35c192176408627ffd4fc68a65fa4b9e1af95f
2405-011-PW <2405-011-pw@U-LDNKOR4ROAZY.corp.amazonworkspaces.com>
2024-06-04 14:38:37 +0100

    finish exercise 0

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,17 @@
 // TODO (Ex.0): document and write tests for this function
-
-function asNegative(n) {
-  // ...
+/**
+ * Given a number, returns a negative version of the number.
+ * 
+ * @param {number} NUM - input number
+ * 
+ * @returns {number} 'NUM' - input number (always negative)
+ */
+function asNegative(NUM) {
+  if (NUM > 0) {
+    return -NUM;
+  } else {
+    return NUM;
+  }
 }
 
 
